687f3305244f40f2e0e6332e12d4045e96f06ddf
kopianb <115834707+kopianb@users.noreply.github.com>
2022-10-15 02:53:21 +0700

    Update gdriveTools.py

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -150,7 +150,7 @@ class GoogleDriveHelper:
         # File body description
         file_metadata = {
             'name': file_name,
-            'description': 'Uploaded by Mirror-leech-telegram-bot',
+            'description': 'Di Upload oleh Tukang Halu',
             'mimeType': mime_type,
         }
         if parent_id is not None:
@@ -408,7 +408,7 @@ class GoogleDriveHelper:
     def __create_directory(self, directory_name, parent_id):
         file_metadata = {
             "name": directory_name,
-            "description": "Uploaded by Mirror-leech-telegram-bot",
+            "description": "Di Upload oleh Tukang Halu",
             "mimeType": self.__G_DRIVE_DIR_MIME_TYPE
         }
         if parent_id is not None:
